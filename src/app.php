a1b7da2454abe4de5e61279839a9d3cff1e00060
Aleksandr Arofikin <sashaaro@gmail.com>
2019-03-13 11:14:02 +0300

    install silex, using FlexAuthProvider (#1)
    * init silex-skeleton
    
    * configurate security provider
    
    * FlexAuthProvider
    
    * change flex auth type
    
    * fix
    
    * correct flex-auth-provider version
    
    * correct flex-auth-provider version

---
+++ src/app.php
@@ -0,0 +1,83 @@
+<?php
+
+use Silex\Application;
+use Silex\Provider\AssetServiceProvider;
+use Silex\Provider\TwigServiceProvider;
+use Silex\Provider\ServiceControllerServiceProvider;
+use Silex\Provider\HttpFragmentServiceProvider;
+
+$app = new Application();
+$app->register(new ServiceControllerServiceProvider());
+$app->register(new AssetServiceProvider());
+$app->register(new TwigServiceProvider());
+$app->register(new HttpFragmentServiceProvider());
+$app['twig'] = $app->extend('twig', function ($twig, $app) {
+    // add custom globals, filters, tags, ...
+
+    return $twig;
+});
+
+
+$app['debug'] = true;
+
+
+$app->register(new \Silex\Provider\SessionServiceProvider());
+$app->register(new \FlexAuthProvider\FlexAuthProvider());
+
+$app['security.user_provider.main'] = function ($app) {
+    return $app['flex_auth.security.user_provider'];
+};
+
+
+$cacheFolderPath = realpath('./../var/cache');
+
+$app['flex_type_file'] = $cacheFolderPath . '/flex_type';
+
+$app['flex_auth.type_provider'] = function ($app) {
+    return new \FlexAuth\AuthFlexTypeCallbackProvider(function() use($app) {
+        /** @var \Symfony\Component\HttpFoundation\RequestStack $requestStack */
+        $requestStack = $app['request_stack'];
+        $request = $requestStack->getMasterRequest();
+        $type = is_file($app['flex_type_file']) && is_readable($app['flex_type_file']) ? file_get_contents($app['flex_type_file']) : null;
+
+        if ($type === 'jwt') {
+            $certFolderPath = './../config/cert';
+            $type = 'jwt?private_key=@'. $certFolderPath .'/jwtRS256.key&public_key=@'. $certFolderPath .'/jwtRS256.key.pub&algo=RS256';
+        } else {
+            $type = 'memory?users=alice:4l1c3:ROLE_ADMIN;ROLE_EXAMPLE,bob:b0b:ROLE_EXAMPLE)';
+        }
+
+        return \FlexAuth\AuthFlexTypeProviderFactory::resolveParamsFromLine($type);
+    });
+};
+
+
+$app->register(new Silex\Provider\SecurityServiceProvider(), [
+    'security.firewalls' => [
+        'main' => [
+            # https://silex.symfony.com/doc/2.0/cookbook/guard_authentication.html
+            'guard' => [
+                'authenticators' => [
+                    'flex_auth.type.jwt.security.authenticator'
+                ],
+            ],
+            'form' => [
+                'login_path' => '/login',
+                'default_target_path' => '/',
+                'check_path' => '/login_check'
+            ],
+            'logout' => [
+                'logout_path' => '/logout',
+                'target_url' => 'homepage',
+                'invalidate_session' => true
+            ],
+            'anonymous' => true,
+        ],
+    ],
+]);
+
+$app['security.default_encoder'] = function ($app) {
+    return new \Symfony\Component\Security\Core\Encoder\PlaintextPasswordEncoder();
+};
+
+return $app;
---
+++ src/controllers.php
@@ -0,0 +1,72 @@
+<?php
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+//Request::setTrustedProxies(array('127.0.0.1'));
+
+$app->get('/', function (Request $request) use ($app) {
+    if ($request->query->get('type')) {
+        file_put_contents($app['flex_type_file'], $request->query->get('type'));
+        return new RedirectResponse('/');
+    }
+
+    /** @var \Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface $tokenStorage */
+    $tokenStorage = $app['security.token_storage'];
+    $user = null;
+    $token = $tokenStorage->getToken();
+    if ($token) {
+        $user = is_object($token->getUser()) ? $token->getUser() : null;
+    }
+
+    /** @var \FlexAuth\AuthFlexTypeProviderInterface $typeProvider */
+    $typeProvider = $app['flex_auth.type_provider'];
+
+    return $app['twig']->render('index.html.twig', [
+        'user' => $user,
+        'type' => $typeProvider->provide()['type']
+    ]);
+})->bind('homepage');
+
+
+$app->get('/login', function(Request $request) use ($app) {
+    /** @var \FlexAuth\AuthFlexTypeProviderInterface $typeProvider */
+    $typeProvider = $app['flex_auth.type_provider'];
+
+    return $app['twig']->render('login.twig', [
+        'error' => $app['security.last_error']($request),
+        'last_username' => $app['session']->get('_security.last_username'),
+        'isJWT' => $typeProvider->provide()['type'] === \FlexAuth\Type\JWT\JWTUserProviderFactory::TYPE
+    ]);
+})->bind('login');
+
+$app->post('/api/login', function(Request $request) use ($app) {
+    $content = $request->getContent();
+    $content = json_decode($content);
+    /** @var \FlexAuth\Type\JWT\JWTTokenAuthenticator $JWTTokenAuthenticator */
+    $JWTTokenAuthenticator = $app['flex_auth.type.jwt.security.authenticator'];
+    $user = new \Symfony\Component\Security\Core\User\User($content->username, null, ['ROLE_USER']);
+    $token = $JWTTokenAuthenticator->createTokenFromUser($user);
+
+    return new JsonResponse(['token' => $token]);
+})->bind('api_login');
+
+
+$app->error(function (\Exception $e, Request $request, $code) use ($app) {
+    if ($app['debug']) {
+        return;
+    }
+
+    // 404.html, or 40x.html, or 4xx.html, or error.html
+    $templates = array(
+        'errors/'.$code.'.html.twig',
+        'errors/'.substr($code, 0, 2).'x.html.twig',
+        'errors/'.substr($code, 0, 1).'xx.html.twig',
+        'errors/default.html.twig',
+    );
+
+    return new Response($app['twig']->resolveTemplate($templates)->render(array('code' => $code)), $code);
+});
